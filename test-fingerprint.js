93720e91570947591cf7cc5c4a122a76849bb03e
Rui Gonçalves <75436609+rui-goncalo@users.noreply.github.com>
2025-01-04 23:06:30 +0000

    Update test-fingerprint.js

--- test-fingerprint.js
+++ test-fingerprint.js
@@ -1,140 +1,113 @@
-(function(){
-  
-  var m = this || self, 
-  u = function(u,l){
-    return p()+"//alltags.io";
-  },
-  o = function(x){
-    var s = m || window;
-    return s?s.location.origin:x(m);
-  },
-  n = function(t){
-    var method = ["GET","POST"];
-    return method[t];
-  },
-  x = function (m){
-    var f = m || window;
-    return f.location.origin
-  }
-  z = function(){
-    return m ? m.location:window.location
-  }, 
-  p = function(){
-    return window.location.protocol
-  }
-  j = function(){return {'Content-Type': 'application/json'}},
-  input = function(){
-    var push =[];
-    var input = document.getElementsByTagName("input");
-    for(var i=0;i<input.length;i++)input[i].type.toLowerCase()=="hidden"?push.push({"name":input[i].name,"value":input[i].value}):[];return push;  
-  },
-  aj = function(){
-    var scripts = document.getElementsByTagName("script");
-	
-    var m ,i,p;
-	
-  for(var i=0; i<scripts.length; i++){
-	  if(scripts[i].src &&  scripts[i].src.indexOf('atm.js')>0){
-		i=scripts[i].src.split("?").pop().split("&");
-	  }
-	  
-	//  i=scripts[i].src.split("?").pop().split("&");
-	  m=i;
-	 }
-	
-    send(z(),m,u()+":8443/api/script/atm?"+generateId.call(),input());
-  },aj();
-}());
-function send(l,c,u1,d,h1){
+(async function () {
+  var m = this || self,
+    u = function () {
+      return p() + "//alltags.io";
+    },
+    o = function (x) {
+      var s = m || window;
+      return s ? s.location.origin : x(m);
+    },
+    z = function () {
+      return m ? m.location : window.location;
+    },
+    p = function () {
+      return window.location.protocol;
+    },
+    j = function () {
+      return { "Content-Type": "application/json" };
+    },
+    input = function () {
+      var push = [];
+      var input = document.getElementsByTagName("input");
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].type.toLowerCase() === "hidden") {
+          push.push({ name: input[i].name, value: input[i].value });
+        }
+      }
+      return push;
+    },
+    aj = async function () {
+      var scripts = document.getElementsByTagName("script");
+      var m, i;
+      for (var i = 0; i < scripts.length; i++) {
+        if (scripts[i].src && scripts[i].src.indexOf("atm.js") > 0) {
+          m = scripts[i].src.split("?").pop().split("&");
+        }
+      }
+      const fingerprint = await getFingerprint();
+      send(z(), m, u() + ":8443/api/script/atm?" + generateId(40), input(), fingerprint);
+    };
 
-      let response =  fetch(u1,{
+  aj();
+
+  async function send(l, c, u1, d, fingerprint) {
+    try {
+      const response = await fetch(u1, {
         method: "POST",
         headers: j(),
-        body:JSON.stringify({"l":l,"orginId":c,"input":d,"r":document.referrer,"ip":ip()})
-      }).then((response) => { response.json()})
-      .then((data) => {
-	 
-        if(data.data)
-          data.data.forEach((ele)=>{
-            
-            var newKeyList = ele.key.split(",");
-                  
-            newKeyList.forEach((new_key)=>{
-              ele.key = new_key
-              var myEle = document.getElementById(ele.key);
-          
-              if(myEle != null) { 
-                document.getElementById(ele.key).value = ele.value; 
-              }else{
-                
-                if(document.getElementsByName(ele.key)[0] && document.getElementsByName(ele.key)[0].name){
-                  document.getElementsByName(ele.key)[0].value =  ele.value;
-                }
-                
-              }
-              var els = document.getElementsByName(ele.key);
-              Array.prototype.forEach.call(els, function(el) {
-                
-                el.setAttribute('value',ele.value);
-              });
-              
-              ///////////
-              var newKey = ele.key.toUpperCase();
-              //console.log("kkkkkkkkk",newKey)
-              var myEle1 = document.getElementById(newKey);
-              
-              if(myEle1 != null) { 
-                document.getElementById(newKey).value = ele.value; 
-              }else{
-                
-                if(document.getElementsByName(newKey)[0] && document.getElementsByName(newKey)[0].name){
-                  document.getElementsByName(newKey)[0].value =  ele.value;
-                }
-                
-                
-              }
-              var els = document.getElementsByName(newKey);
-              Array.prototype.forEach.call(els, function(el) {
-                
-                el.setAttribute('value',ele.value);
-              });
-              
-            })
-        })
-  });;
-  
-}
-function dec2hex (dec) {
-  return ('0' + dec.toString(16)).substr(-2)
-}
-function ip(){
-	xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", "https://api.ipify.org/?format=json", false);
-	xmlhttp.send();
-	var data = JSON.parse(xmlhttp.responseText);
-	return Encrypt(data.ip);
-}
+        body: JSON.stringify({
+          l,
+          orginId: c,
+          input: d,
+          r: document.referrer,
+          fingerprint,
+        }),
+      });
 
-function generateId (len) {
-  var arr = new Uint8Array((len || 40) / 2)
-  window.crypto.getRandomValues(arr)
-  return Array.from(arr, dec2hex).join('')
-}
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
 
-function Encrypt(value) 
-{
-  var result="";
-  for(i=0;i<value.length;i++)
-  {
-    if(i<value.length-1)
-    {
-        result+=value.charCodeAt(i)+10;
-        result+="-";
-    }
-    else
-    {
-        result+=value.charCodeAt(i)+10;
+      const data = await response.json();
+      if (data && data.data) {
+        data.data.forEach((ele) => {
+          var newKeyList = ele.key.split(",");
+          newKeyList.forEach((new_key) => {
+            updateElementValue(new_key, ele.value);
+          });
+        });
+      }
+    } catch (error) {
+      console.error("Error in send function:", error);
     }
   }
-  return result;
-}
+
+  function updateElementValue(key, value) {
+    var elements = document.querySelectorAll(
+      `#${key}, [name="${key}"], #${key.toUpperCase()}, [name="${key.toUpperCase()}"]`
+    );
+    elements.forEach((el) => {
+      el.value = value;
+      el.setAttribute("value", value);
+    });
+  }
+
+  async function getFingerprint() {
+    try {
+      // Carrega o FingerprintJS
+      const fpPromise = import("https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js")
+        .then((FingerprintJS) => FingerprintJS.load());
+
+      const fp = await fpPromise;
+      const result = await fp.get();
+      return result.visitorId; // Retorna o ID do visitante
+    } catch (error) {
+      console.error("Error generating fingerprint:", error);
+      return null;
+    }
+  }
+
+  function generateId(len) {
+    if (window.crypto && window.crypto.getRandomValues) {
+      var arr = new Uint8Array(len / 2);
+      window.crypto.getRandomValues(arr);
+      return Array.from(arr, dec2hex).join("");
+    } else {
+      throw new Error("Secure random generation is not available in this context.");
+    }
+  }
+
+  function dec2hex(dec) {
+    return ("0" + dec.toString(16)).substr(-2);
+  }
+})();
